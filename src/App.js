9274b35b24936a1518f6742b9d1dbf15714a2665
Daniil Potapov <potapovdg@pik.ru>
2019-09-27 19:39:14 +0300

    refactor app

--- src/App.js
+++ src/App.js
@@ -1,284 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Highcharts from 'highcharts';
 import data from './data.json';
-import { Table } from 'antd';
-
-import HighchartsReact from 'highcharts-react-official';
-
-const options =  {
-  chart: {
-      zoomType: 'xy'
-  },
-  title: {
-      text: 'Average Monthly Weather Data for Tokyo',
-      align: 'left'
-  },
-  subtitle: {
-      text: 'Source: WorldClimate.com',
-      align: 'left'
-  },
-  xAxis: [{
-      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-          'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-      crosshair: true
-  }],
-  yAxis: [{ // Primary yAxis
-      labels: {
-          format: '{value}°C',
-          style: {
-              color: Highcharts.getOptions().colors[2]
-          }
-      },
-      title: {
-          text: 'Temperature',
-          style: {
-              color: Highcharts.getOptions().colors[2]
-          }
-      },
-      opposite: true
-
-  }, { // Secondary yAxis
-      gridLineWidth: 0,
-      title: {
-          text: 'Rainfall',
-          style: {
-              color: Highcharts.getOptions().colors[0]
-          }
-      },
-      labels: {
-          format: '{value} mm',
-          style: {
-              color: Highcharts.getOptions().colors[0]
-          }
-      }
-
-  }, { // Tertiary yAxis
-      gridLineWidth: 0,
-      title: {
-          text: 'Sea-Level Pressure',
-          style: {
-              color: Highcharts.getOptions().colors[1]
-          }
-      },
-      labels: {
-          format: '{value} mb',
-          style: {
-              color: Highcharts.getOptions().colors[1]
-          }
-      },
-      opposite: true
-  }],
-  tooltip: {
-      shared: true
-  },
-  legend: {
-      layout: 'vertical',
-      align: 'left',
-      x: 80,
-      verticalAlign: 'top',
-      y: 55,
-      floating: true,
-      backgroundColor:
-          Highcharts.defaultOptions.legend.backgroundColor || // theme
-          'rgba(255,255,255,0.25)'
-  },
-  series: [{
-      name: 'Rainfall',
-      type: 'spline',
-      yAxis: 1,
-      marker: {
-          enabled: true
-      },
-      data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
-      tooltip: {
-          valueSuffix: ' mm'
-      }
-
-  }, {
-      name: 'Sea-Level Pressure',
-      type: 'spline',
-      yAxis: 2,
-      data: [1016, 1016, 1015.9, 1015.5, 1012.3, 1009.5, 1009.6, 1010.2, 1013.1, 1016.9, 1018.2, 1016.7],
-      marker: {
-          enabled: true
-      },
-      dashStyle: 'shortdot',
-      tooltip: {
-          valueSuffix: ' mb'
-      }
-
-  }, {
-      name: 'Temperature',
-      type: 'spline',
-      marker: {
-        enabled: true
-      },
-      data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6],
-      tooltip: {
-          valueSuffix: ' °C'
-      }
-  }],
-  responsive: {
-      rules: [{
-          condition: {
-              maxWidth: 500
-          },
-          chartOptions: {
-              legend: {
-                  floating: false,
-                  layout: 'horizontal',
-                  align: 'center',
-                  verticalAlign: 'bottom',
-                  x: 0,
-                  y: 0
-              },
-              yAxis: [{
-                  labels: {
-                      align: 'right',
-                      x: 0,
-                      y: -6
-                  },
-                  showLastLabel: false
-              }, {
-                  labels: {
-                      align: 'left',
-                      x: 0,
-                      y: -6
-                  },
-                  showLastLabel: false
-              }, {
-                  visible: false
-              }]
-          }
-      }]
-  }
-}
-
-const opt = [
-  {
-    "Channel": "Display Network",
-    "Date": "2019-06-12T00:00:00.000Z",
-    "Media Source": "googleadwords_int",
-    "ADS_FIVE_WATCHED": 71.0,
-    "ADS_VIDEOAD_WATCHED": 2134.0,
-    "Add_Friend_Request": 0.0,
-    "Click_Gifts": 0.0,
-    "Click_Messages": 0.0,
-    "CompleteGames": 0.0,
-    "Complete_Send_Gifts": 0.0,
-    "LAUNCH_REGISTRATION_COMPLETED": 544.0,
-    "PRESTIGE_RESET_ACCEPTED": 0.0,
-    "SCREEN_LEAGUE_COMPLETED": 107.0,
-    "SHOP_PURCHASE_ADDED": 4.0,
-    "SHOP_PURCHASE_UNFINISHED": 21.0,
-    "Start_PK_Game": 0.0,
-    "TUTORIAL_STEP_COMPLETED": 88.0,
-    "UNIQUE_FIRST_PURCHASE": 3.0,
-    "Visit_Other_Profile": 0.0,
-    "add_friend": 0.0,
-    "entergame": 0.0,
-    "install": 568.0,
-    "loginsuccess": 0.0,
-    "stage_completed": 1012.0,
-    "ten_games": 0.0,
-    "SHOP_PURCHASE_ADDED revenue, USD": 10.96,
-    "UNIQUE_FIRST_PURCHASE revenue, USD": 0.0,
-    "Campaign": "campaign_1",
-    "Impressions": 554471.0,
-    "Clicks": 6950.0,
-    "Cost": 99.1456,
-    "Partner": "SOCIARO",
-    "Platform": "Android"
-  },
-  {
-    "Channel": "Display Network",
-    "Date": "2019-06-12T00:00:00.000Z",
-    "Media Source": "googleadwords_int",
-    "ADS_FIVE_WATCHED": 0.0,
-    "ADS_VIDEOAD_WATCHED": 1.0,
-    "Add_Friend_Request": 0.0,
-    "Click_Gifts": 0.0,
-    "Click_Messages": 0.0,
-    "CompleteGames": 0.0,
-    "Complete_Send_Gifts": 0.0,
-    "LAUNCH_REGISTRATION_COMPLETED": 7.0,
-    "PRESTIGE_RESET_ACCEPTED": 0.0,
-    "SCREEN_LEAGUE_COMPLETED": 1.0,
-    "SHOP_PURCHASE_ADDED": 0.0,
-    "SHOP_PURCHASE_UNFINISHED": 0.0,
-    "Start_PK_Game": 0.0,
-    "TUTORIAL_STEP_COMPLETED": 1.0,
-    "UNIQUE_FIRST_PURCHASE": 0.0,
-    "Visit_Other_Profile": 0.0,
-    "add_friend": 0.0,
-    "entergame": 0.0,
-    "install": 7.0,
-    "loginsuccess": 0.0,
-    "stage_completed": 19.0,
-    "ten_games": 0.0,
-    "SHOP_PURCHASE_ADDED revenue, USD": 0.0,
-    "UNIQUE_FIRST_PURCHASE revenue, USD": 0.0,
-    "Campaign": "campaign_65",
-    "Impressions": 1414.0,
-    "Clicks": 20.0,
-    "Cost": 5.1302,
-    "Partner": "SOCIARO",
-    "Platform": "Android",
-  },
-];
-
-const columns = [
-    {
-        title: 'Campaign',
-        dataIndex: 'Campaign',
-        key: 'Campaign'
-    },
-    {
-        title: 'Channel',
-        dataIndex: 'Channel',
-        key: 'Channel'
-    },
-    {
-        title: 'Date',
-        dataIndex: 'Date',
-        key: 'Date'
-    },
-    {
-        title: 'Media Source',
-        dataIndex: 'Media Source',
-        key: 'Media Source'
-    },
-    {
-        title: 'ADS_FIVE_WATCHED',
-        dataIndex: 'ADS_FIVE_WATCHED',
-        key: 'ADS_FIVE_WATCHED'
-    },
-    {
-        title: 'ADS_VIDEOAD_WATCHED',
-        dataIndex: 'ADS_VIDEOAD_WATCHED',
-        key: 'ADS_VIDEOAD_WATCHED'
-    },
-    {
-        title: 'Add_Friend_Request',
-        dataIndex: 'Add_Friend_Request',
-        key: 'Add_Friend_Request'
-    },
-]
-
-
-
+import Root from './components/Root';
 
 function App() {
+  const formatData = data.reduce((acc, current) => {
+    if (typeof current['Campaign'] === 'object') {
+      return acc;
+    }
+    let date = new Date(Date.parse(current['Date']));
+    date = new Intl.DateTimeFormat().format(date);
+    acc.push({ ...current, ForamtDate: date, DateMs: +new Date(current['Date']) });
+    return acc;
+  }, []);
+
+  const normData = formatData.reduce((acc, current) => {
+    if (typeof current['Campaign'] === 'object') {
+      return acc;
+    }
+
+    if (!Array.isArray(acc[current['Campaign']])) {
+      acc[current['Campaign']] = [];
+    }
+
+    acc[current['Campaign']].push(current);
+
+    return acc;
+  }, {});
+
+  console.log('normData', formatData, normData);
   return (
     <div className="App">
-      <HighchartsReact
-        highcharts={Highcharts}
-        options={options}
-      />
-
-    <Table columns={columns} dataSource={data} onChange={() => {}} />
+      <Root normData={normData} formatData={formatData} />
     </div>
   );
 }
